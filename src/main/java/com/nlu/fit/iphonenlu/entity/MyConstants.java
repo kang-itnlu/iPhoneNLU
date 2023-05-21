d5c3d6ce448cb6a64c2755fd7f55b863c93d5aad
ndkhang_java <ndkhang.itnlu@gmail.com>
2023-05-21 13:49:27 +0700

    Change UI and Config upload file

--- src/main/java/com/nlu/fit/iphonenlu/entity/MyConstants.java
+++ src/main/java/com/nlu/fit/iphonenlu/entity/MyConstants.java
@@ -2,8 +2,8 @@ package com.nlu.fit.iphonenlu.entity;
 
 public class MyConstants {
 	// Replace with your email here:  
-	   public static final String MY_EMAIL = "";
+	   public static final String MY_EMAIL = "19130096@st.hcmuaf.edu.vn";
 	   
 	    // Replace password!!
-	    public static final String MY_PASSWORD = "";
+	    public static final String MY_PASSWORD = "KhanG3301@";
 }
